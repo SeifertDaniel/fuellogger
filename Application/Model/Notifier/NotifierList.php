a73a3d931f7d62d8deb969f82770c0830880ad1f
Daniel Seifert <ds@shopmodule.com>
2022-03-22 09:06:24 +0100

    add DailyBestPrice filter

--- Application/Model/Notifier/NotifierList.php
+++ Application/Model/Notifier/NotifierList.php
@@ -5,6 +5,7 @@ namespace Daniels\Benzinlogger\Application\Model\Notifier;
 use Daniels\Benzinlogger\Application\Model\Fuel;
 use Daniels\Benzinlogger\Application\Model\Notifier\ConcreteNotifier\Ifttt;
 use Daniels\Benzinlogger\Application\Model\Notifier\ConcreteNotifier\WhatsApp;
+use Daniels\Benzinlogger\Application\Model\NotifyFilters\DailyBestPriceFilter;
 use Daniels\Benzinlogger\Application\Model\NotifyFilters\FuelTypeFilter;
 use Daniels\Benzinlogger\Application\Model\NotifyFilters\TimeFilter;
 
@@ -16,8 +17,9 @@ class NotifierList
             new DebugNotifier(),
             (new Ifttt($_ENV['C001_IFTTT_URL'])),
             (new WhatsApp($_ENV['C001_WHAPP_PHONE'], $_ENV['C001_WHAPP_APIK']))
-                ->setFilter(new TimeFilter('08:00:00', '23:00:00'))
+                ->setFilter(new TimeFilter('08:00:00', '22:00:00'))
                 ->setFilter(new FuelTypeFilter([Fuel::TYPE_E10]))
+                ->setFilter(new DailyBestPriceFilter())
         ];
 
         return $list;
---
+++ Application/Model/NotifyFilters/DailyBestPriceFilter.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * This Software is the property of Data Development and is protected
+ * by copyright law - it is NOT Freeware.
+ * Any unauthorized use of this software without a valid license
+ * is a violation of the license agreement and will be prosecuted by
+ * civil and criminal law.
+ * http://www.shopmodule.com
+ *
+ * @copyright (C) D3 Data Development (Inh. Thomas Dartsch)
+ * @author        D3 Data Development - Daniel Seifert <support@shopmodule.com>
+ * @link          http://www.oxidmodule.com
+ */
+
+namespace Daniels\Benzinlogger\Application\Model\NotifyFilters;
+
+use Daniels\Benzinlogger\Application\Model\DBConnection;
+use Daniels\Benzinlogger\Application\Model\Price;
+use Doctrine\DBAL\Exception;
+
+class DailyBestPriceFilter extends AbstractFilter
+{
+    /**
+     * @param       $fuelType
+     * @param float $price
+     *
+     * @return bool
+     * @throws Exception
+     */
+    public function canNotifiy( $fuelType, float $price ): bool
+    {
+        return $price < $this->getBestPriceBeforeUpdate($fuelType);
+    }
+
+    /**
+     * @param $fuelType
+     *
+     * @return float
+     * @throws Exception
+     */
+    public function getBestPriceBeforeUpdate($fuelType): float
+    {
+        $qb = DBConnection::getConnection()->createQueryBuilder();
+        $qb->select('pr.price')
+            ->from((new Price())->getCoreTableName(), 'pr')
+            ->where(
+                $qb->expr()->and(
+                    'pr.datetime BETWEEN DATE_FORMAT(NOW(), "%Y-%m-%d 00:00:00") AND DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 2 MINUTE), "%Y-%m-%d %H:%i:%s")',
+                    $qb->expr()->eq(
+                        'pr.type',
+                        $qb->createNamedParameter($fuelType)
+                    )
+                )
+            )
+            ->orderBy('pr.price', 'ASC')
+            ->setMaxResults(1);
+        return (float) $qb->fetchOne();
+    }
+}
