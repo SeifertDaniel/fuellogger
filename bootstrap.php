b36f55000fcbc185e524d1f59ec18cc643ea4e7d
Daniel Seifert <git@daniel-seifert.com>
2022-04-02 22:42:37 +0200

    improve display errors settings

--- bootstrap.php
+++ bootstrap.php
@@ -3,14 +3,20 @@
 use Daniels\FuelLogger\Core\Debug;
 use Dotenv\Dotenv;
 
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
-ini_set('display_errors', 0);
 define('INSTALLATION_ROOT_PATH', dirname(__DIR__));
 const BASE_PATH = INSTALLATION_ROOT_PATH . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
 const VENDOR_PATH = INSTALLATION_ROOT_PATH . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR;
 const TMP_PATH = INSTALLATION_ROOT_PATH . DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR;
 
 require_once VENDOR_PATH.'autoload.php';
+
+if (Debug::displayErrors()) {
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL & ~E_DEPRECATED);
+} else {
+    ini_set('display_errors', 0);
+    error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+}
 
 $dotenv = Dotenv::createImmutable(__DIR__."/..");
 $dotenv->load();
@@ -26,10 +32,5 @@ $dotenv->required([
     'LOCATIONLNG',
     'COMMODITIESAPIKEY'
 ])->notEmpty();
-
-if (Debug::displayErrors()) {
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL & ~E_DEPRECATED);
-}
 
 require_once BASE_PATH . 'functions.php';
