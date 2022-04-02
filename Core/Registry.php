14f8f12e9e9ad396d9aa39f4b5bc6810e507a202
Daniel Seifert <git@daniel-seifert.com>
2022-04-03 00:51:26 +0200

    fix debug option

--- Core/Registry.php
+++ Core/Registry.php
@@ -58,7 +58,7 @@ class Registry
             $loader = new FilesystemLoader($_ENV['ROOTDIR'].'src/Templates');
             $twig = new Environment($loader, [
                 'cache' => $_ENV['ROOTDIR'].'tmp/twig',
-                'debug' => Debug::useTwigCaching()
+                'debug' => !Debug::useTwigCaching()
             ]);
 
             self::set('twig', $twig);
